fcb74f8760fa808769e6303bc15c980ded91bcda
Jude Sajith <65233121+SajithJude@users.noreply.github.com>
2022-12-06 22:01:59 +0000

    zx

--- bg_remove.py
+++ bg_remove.py
@@ -81,9 +81,9 @@ my_upload = st.sidebar.file_uploader("Upload an image", type=["png", "jpg", "jpe
 # uploaded_files = st.file_uploader("Choose a CSV file", accept_multiple_files=True)
 st.write(imagear)
 for uploaded_file in my_upload:
-    bytes_data = uploaded_file.read()
-    st.write("filename:", uploaded_file.name)
-    st.write(bytes_data)
+    # bytes_data = uploaded_file.read()
+    # st.write("filename:", uploaded_file.name)
+    # st.write(bytes_data)
     imagear.append(Image.open(uploaded_file))
 
 st.write(imagear)
