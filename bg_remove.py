08bc176b72772f445d1ec0b8c625df6f2d53cebb
Jude Sajith <65233121+SajithJude@users.noreply.github.com>
2022-12-06 21:09:40 +0000

    D

--- bg_remove.py
+++ bg_remove.py
@@ -105,7 +105,7 @@ col1, col2 = st.columns(2)
 my_upload = st.sidebar.file_uploader("Upload an image", type=["png", "jpg", "jpeg"])
 
 if my_upload is not None:
-    fix_image(upload=my_upload)
+    # fix_image(upload=my_upload)
 else:
     join_images(
 *images,
